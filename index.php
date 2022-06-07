492821b687591efd96f6b3b4d4f4ce76e41085a5
Alejandro Burguillos Guisado <aburgui115a@ieslacampina.es>
2022-06-07 11:08:07 +0200

    Cambios 2

--- index.php
+++ index.php
@@ -2,12 +2,45 @@
 <html lang="es">
 <link rel="icon" type="image/x-icon" href="liga.jpeg">
 <head>
+<meta charset="UTF-8">
     <title>X Liga 3x3 Cele Cortés</title>
 </head>
 <body>
+
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pwd = "iaw";
+
+    $enlace = mysqli_connect($host, $user, $pwd);
+    mysqli_select_db($enlace, "liga");
+?>
+
+<form action="" method="post">
+    <?php
+        session_start();
+    ?>
+
     <h1>Bienvenidos a la Liga 3x3 Cele Cortés</h1>
     <img src="liga.jpeg">
+    <p>Bienvenidos una vez más a otro liga 3x3 organizada por el Club Baloncesto Paradas</p>
+    <p>¿Qué desea realizar?</p>
     
+    <p>
+        <input type="submit" name="equipo" value="Inscribir Equipo">
+        <input type="submit" name="mesa" value="Isncribir Mesa">
+        <input type="submit" name="partido" value="Rellenar Estadística">
+    </p>
+
+    <?php
+        if(isset($_POST["partido"])){
+        header ('Location: partidos.php');
+        }
+    ?>
+
+
+
+</form>
     
 </body>
 </html>
---
+++ partidos.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es">
+<link rel="icon" type="image/x-icon" href="liga.jpeg">
+<head>
+    <meta charset="UTF-8">
+    <title>Partidos</title>
+</head>
+<body>
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pwd = "iaw";
+
+    $enlace = mysqli_connect($host, $user, $pwd);
+    mysqli_select_db($enlace, "liga");
+?>
+
+<form action="" method="post">
+    <?php
+        session_start();
+    ?>
+    <p>Código del Partido:</p>
+    <input type="text" name="test" value="<?php echo rand(1,1000000000)?>">
+
+    <p>Puntos Equipo Local:</p>
+    <input type="number" name="plocal">
+
+    <p>Puntos Equipo Visitante:</p>
+    <input type="number" name="pvisi">
+
+    <p>Rebotes Equipo Local:</p>
+    <input type="number" name="rlocal">
+
+    <p>Rebotes Equipo Visitante:</p>
+    <input type="number" name="rvisi">
+
+    <p>Asistencias Equipo Local:</p>
+    <input type="number" name="alocal">
+
+    <p>Asistencias Equipo Visitante:</p>
+    <input type="number" name="avisi">
+
+    <p>Fecha del Partido:</p>
+    <input type="date" name="fecha">
+
+
+</form>
+    
+</body>
+</html>
